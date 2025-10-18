29ca8753c2122affacbc4ae569cb5feb03af2eac
Subham Kumar Sinha <subhamsinha9206@gmail.com>
2025-10-18 20:25:46 +0530

    implement fourSum method to find all unique quadruplets that sum to a given target

--- 27. 4 Sum /Solution.java
+++ 27. 4 Sum /Solution.java
@@ -4,42 +4,52 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-public class Solution {
-    public List<List<Integer>> threeSum(int[] nums) {
-        //if size < 3 
-        if(nums==null||nums.length<3) return new ArrayList<>();
+class Solution {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        
+    
+        //if size <  4
+        if(nums==null||nums.length<4) return new ArrayList<>();
         Arrays.sort(nums);
         Set<List<Integer>> set=new HashSet<>();
 
 
-        for (int i = 0; i < nums.length-2; i++) {
-            int left=i+1;
-            int right=nums.length-1;
-            while ((left<right)) {
-                int sum=nums[i]+nums[left]+nums[right];
-                if(sum==0){
-                    set.add(Arrays.asList(nums[i],nums[left],nums[right]));
-                    left++;
-                    right--;
-
-                }else if(sum<0){
-                    left++;
+        for (int i = 0; i < nums.length-3; i++) {
+            for (int j =i+1; j < nums.length-2; j++) {
+                int left=j+1;
+                int right=nums.length-1;
+                while(left<right){
+                    long sum=(long) nums[i]+nums[j]+nums[left]+nums[right];
+                    if(sum==target){
+                        set.add(Arrays.asList(nums[i],nums[j],nums[left],nums[right]));
+                        left++;
+                        right--;
 
 
-                }else{
-                    right--;
+
+                    }else if(sum<target){
+                        left++;
+                        
+                    }else{
+                        right--;
+                    }
                 }
+ 
+            
                 
             }
+           
+            
             
         }
 
     return new ArrayList<>(set);
     }
+
     public static void main(String[] args) {
-        int arr[]={-1,0,1,2,-1,-4};
-         List<List<Integer>> se= new Solution().threeSum(arr);
-         System.out.println(se);
-        
+        int arr[] = {1000000000,1000000000,1000000000,1000000000};
+        List<List<Integer>> se = new Solution().fourSum(arr, -294967296);
+        System.out.println(se);
+
     }
 }
